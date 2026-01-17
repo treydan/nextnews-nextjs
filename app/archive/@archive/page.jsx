da9f953b17c951c9fbf9dce7907974075e5f6db1
Trey Jamison <trey.jamison@gmail.com>
2026-01-17 15:22:26 -0600

    setup parallel routes

---
+++ app/archive/@archive/page.jsx
@@ -0,0 +1,5 @@
+const ArchivePage = () => {
+  return <h1>Archive Page</h1>;
+};
+
+export default ArchivePage;
---
+++ app/archive/@latest/page.jsx
@@ -0,0 +1,5 @@
+const LatestNewsPage = () => {
+  return <h1>Latest News</h1>;
+};
+
+export default LatestNewsPage;
