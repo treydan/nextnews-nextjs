5340ee0d5d49ac2be67f2649b3fe919a97e4d81c
Trey Jamison <trey.jamison@gmail.com>
2026-01-19 12:57:57 -0600

    get news from separate backend

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,18 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-const NewsPage = () => {
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList newsItems={DUMMY_NEWS} />
+      <NewsList newsItems={news} />
     </>
   );
 };
