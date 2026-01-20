e038e9282923cfa87f677be9ef4f7411632fcd3c
Trey Jamison <trey.jamison@gmail.com>
2026-01-20 13:03:58 -0600

    setup local db

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,13 +1,8 @@
+import { getAllNews } from "@/lib/news";
 import NewsList from "@/components/news-list";
 
 const NewsPage = async () => {
-  const response = await fetch("http://localhost:8080/news");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
-
-  const news = await response.json();
+  const news = getAllNews();
 
   return (
     <>
